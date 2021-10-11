c1ad4b149d94f5875c3d719c0eeb684821148aee
Linda Vanessa Zapata Henao <linda.zapatah@perficient.com>
2021-10-10 20:44:28 -0500

    refactor(NotFound): Add style to the component

--- src/components/NotFound/index.jsx
+++ src/components/NotFound/index.jsx
@@ -1,9 +1,19 @@
-import React from "react";
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './styles.scss'
 
-const NotFound = () => {
-    return (
-        <div>Not Found</div>
-    )
-}
+export const NotFound = () => {
+   return (
+      <div className="not-found">
+         <img
+            src="https://i.pinimg.com/originals/86/41/80/86418032b715698a4dfa6684b50c12af.gif"
+            alt=""
+         />
+         <Link to="/">
+            <h1>Return to Home</h1>
+         </Link>
+      </div>
+   );
+};
 
 export default NotFound;
