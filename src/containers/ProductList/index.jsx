6cd48357cecebb7acbdf8c151eee677ae8177652
Linda Vanessa Zapata Henao <linda.zapatah@perficient.com>
2021-10-12 00:09:50 -0500

    refactor(ProductList): Add integration with API

--- src/containers/ProductList/index.jsx
+++ src/containers/ProductList/index.jsx
@@ -1,12 +1,25 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import ProductItem from '@components/ProductItem';
+import Axios from 'axios';
+
 import './styles.scss';
 
+const API_URL = 'https://api.escuelajs.co/api/v1/products';
+
 const ProductList = () => {
+    const [ products, setProducts ] = useState([]);
+
+    useEffect(async () => {
+       const productResponse = await Axios(API_URL);
+       setProducts(productResponse.data);
+    }, [])
+
 	return (
 		<section className="main-container">
 			<div className="ProductList">
-				<ProductItem />
+                {
+                    products.map(product => <ProductItem /> )
+                }
 			</div>
 		</section>
 	);
